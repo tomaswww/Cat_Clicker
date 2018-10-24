17ca10ac8cd7d23c4237351a601079236d954206
tomaswww <twingord@gmail.com>
2018-10-24 19:57:11 -0300

    Added two pictures of cats for second requirement

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-var clickCount=0;
-var kittenPhoto = document.querySelector(".photo");
-kittenPhoto.addEventListener("click", function(){
-  clickCount++;
-  if (clickCount===1){
-  let changeCount = document.querySelector(".board");
-  changeCount.innerHTML = ("You made "+clickCount+" click");
-}
-else{
-  let changeCount = document.querySelector(".board");
-  changeCount.innerHTML = ("You made "+clickCount+" clicks");
-}
-}, false);
+document.body.innerHTML = "";
+// Here we put cats on an Array
+var catsArray = ["mickeyTheCat", "willyTheCat"];
+// Loop over array to define cats
+for (var i = 0; i < catsArray.length; i++) {
+  var num = i;
+  // create image for new cat
+  var newImage = document.createElement("img");
+  newImage.setAttribute("src", "images/kitten.jpg");
+  newImage.className = "photo";
+  // create click events
+  newImage.addEventListener("click", (function(clickCopy){
+    return function(){
+      clickCopy++;
+      if (clickCopy===1){
+        let changeCount = document.querySelector(".board");
+        changeCount.innerHTML = ("You made "+clickCopy+" click");
+      }
+      else{
+        let changeCount = document.querySelector(".board");
+        changeCount.innerHTML = ("You made "+clickCopy+" clicks");
+      }
+    }
+  })(num));
+  // append the new cat to a div section
+  document.body.appendChild(newImage);
+};
 
 
 
@@ -21,31 +35,18 @@ else{
 
 
 
-
-
-// var catsArray = ["johnTheCat", "marcellusTheCat", "walterTheCat", "mickyTheCat", "oswaldTheCat"];
-// var clicksMade = 0;
-// var num = 0;
-// for (var i = 0; i < catsArray.length; i++) {
-// var newCatDiv = document.createElement("div");
-//   num++;
-//   // crreate images div
-//   var newCatDiv = document.createElement("div");
-//   // create an image element
-//   var newCat = document.createElement("img");
-//   // set the image src
-//   newCat.setAttribute("src", "images/kitten.jpg");
-//   // put the name to the image
-//   // newCat.textContent = catsArray[i];
-//   // // set the elements id
-//   // newCat.id = catsArray[i];
-//   // // add the event listener
-//   // newCat.addEventListener('click', (function(numCopy) {
-//   //   clicksMade++;
-//   //   return function() {
-//   //     var catId = newCat.id;
-//   //     newCat.innerHTML("You Made " + clicksMade + " Click/s");
-//   //   }
-//   // })(num));
-// body.appendChild(newCatDiv);
+//
+//
+// var clickCount=0;
+// var kittenPhoto = document.querySelector(".photo");
+// kittenPhoto.addEventListener("click", function(){
+//   clickCount++;
+//   if (clickCount===1){
+//   let changeCount = document.querySelector(".board");
+//   changeCount.innerHTML = ("You made "+clickCount+" click");
 // }
+// else{
+//   let changeCount = document.querySelector(".board");
+//   changeCount.innerHTML = ("You made "+clickCount+" clicks");
+// }
+// }, false);
