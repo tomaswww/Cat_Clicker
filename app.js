a94a0a6af0c8892bb41dfb0d998999f97d23856a
tomaswww <twingord@gmail.com>
2018-10-29 19:28:31 -0300

    set up catPanel based on octopus current Cat

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var model ={
   name: "Willy",
   photo: "images/willyTheCat.jpg",
   clickCount: 0
-}];
+}]};
 
 
 
@@ -45,85 +45,32 @@ incrementCounter: function(){
 };
 
 
-// get from view 1 the cat selected
-// set the view 2 with data
-
-// Here we call the nav menu to display
-displayCat();
-};
 
   // VIEW 1:
   // SideBar for cat Menu
   // create de side bar :
-  function displayCat(){
-  for (var i = 0; i < catsArray.length; i++) {
-    var newLink = document.createElement("a");
-    newLink.setAttribute("href", "#");
-    newLink.innerHTML = (catsArray[i].name);
-    var sideBar = document.getElementsByClassName("links");
-    sideBar.appendChild(newLink);
-  };
-};
 
 
   // VIEW 2:
   // Display area for current cat
-
-
-  // OLD CODE TO REARANGE:
-
-  // Loop over array to define cats
-
-
-
-//   // create new container
-//   var container = document.createElement("div");
-//   container.className = "container";
-//   container.classList.add("hidden");
-//   container.id = catsArray[i].name;
-//   // add click feature to show only clicked kitten
-//   newLink.addEventListener("click", (function(containerToShow) {
-//     for (let i = 0; i < catsArray.lenght; i++) {
-//       var toHide = document.getElementsByClassName("show");
-//       if (toHide.lenght > 0) {
-//         toHide.classList.remove("show");
-//         toHide.classList.add("hidden");
-//       }
-//     };
-//     return function() {
-//       containerToShow.classList.remove("hidden");
-//       containerToShow.classList.add("show");
-//     }
-//   })(container));
-//   sideBar.appendChild(newLink);
-//   // stablish cats cats name and title
-//   var nameC = catsArray[i].name;
-//   var titleName = document.createElement("div");
-//   titleName.innerHTML = (catsArray[i].name + " the cat");
-//   // create new board for clicks to be shown
-//   var board = document.createElement("div");
-//   board.className = catsArray[i].board;
-//   board.innerHTML = "You can click on " + catsArray[i].name + " the cat";
-//   // create image for new cat
-//   var newImage = document.createElement("img");
-//   newImage.setAttribute("src", catsArray[i].photo);
-//   newImage.className = "photo";
-//   // create click events
-//   newImage.addEventListener("click", (function(clickCopy, nBoard, catsName) {
-//     return function() {
-//       clickCopy++;
-//       if (clickCopy === 1) {
-//         nBoard.innerHTML = ("You made " + clickCopy + " click on " + catsName);
-//       } else {
-//         nBoard.innerHTML = ("You made " + clickCopy + " clicks on " + catsName);
-//       }
-//     }
-//   })(num, board, nameC));
-//   // append the new cat to a div section
-//   container.appendChild(titleName);
-//   container.appendChild(newImage);
-//   container.appendChild(board);
-//   document.body.appendChild(container);
-//   document.body.appendChild(sideBar);
-// };
-// };
+var catPanel ={
+  init: function(){
+    // set shortcut for selecting elements in DOM
+    this.clickPanel = document.getElementById("clicksDone");
+    this.catPic = document.getElementById("photo");
+    this.catNam = document.getElementById("kittenName");
+    // set the event listener for picture of kittenName
+    this.catPic.addEventListener("click", function(){
+      octopus.incrementCounter():
+    });
+    // render new view of container
+    this.render();
+  },
+  render: function(){
+    // it updates the DOM view
+    var currentCat = octopus.defineCurrentCat();
+    this.clickPanel.textContent = currentCat.clickCount;
+    this.catPic.src = currentCat.photo;
+    this.catNam = currentCat.name;
+  }
+};
