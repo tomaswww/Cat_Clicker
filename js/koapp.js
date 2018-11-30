96030161942b16c6e7097708e254afc2dbcd5841
tomaswww <twingord@gmail.com>
2018-11-30 12:03:04 -0300

    added code for catlist in koapp js

--- js/koapp.js
+++ js/koapp.js
@@ -1,16 +1,48 @@
-var Cat = function(){
-this.name= ko.observable("Willy");
-this.photo= ko.observable("images/willyTheCat.jpg");
-this.clickCount= ko.observable(0);
+var initialCats = [
+  {
+      name: "Mickey",
+      photo: "images/mickeyTheCat.jpg",
+      clickCount: 0
+    },
+    {
+      name: "Willy",
+      photo: "images/willyTheCat.jpg",
+      clickCount: 0
+    },
+    {
+      name: "Lucky",
+      photo: "images/luckyTheCat.jpg",
+      clickCount: 0
+    },
+    {
+      name: "Johnny",
+      photo: "images/johnnyTheCat.jpg",
+      clickCount: 0
+    },
+    {
+      name: "Jimmy",
+      photo: "images/jimmyTheCat.png",
+      clickCount: 0
+    }
+  ];
+
+
+var Cat = function(data) {
+  this.name = ko.observable(data.name);
+  this.photo = ko.observable(data.photo);
+  this.clickCount = ko.observable(data.clickCount);
 };
 
 
+var ViewModel = function() {
+  this.currentCat = ko.observable(new Cat({
+    name: "Mickey",
+    photo: "images/mickeyTheCat.jpg",
+    clickCount: 0
+  }));
 
-
-var ViewModel = function(){
-  var currentCat = ko.observable( new Cat());
-  this.incrementCounter = function(){
-    this.clickCount(this.clickCount()+1);
+  this.incrementCounter = function() {
+    this.clickCount(this.clickCount() + 1);
   };
 }
 
